1350d5c40bf4d49065947c099903eb15fd9b7be4
mosecund <mohammad.ik.secundar@ulb.be>
2021-12-11 09:40:43 +0100

    J'ai modifié les conversions. Pour l'ip du serveur même si c'est un string c'est bon par contre pour le port j'ai du faire une conversion parce qu'on a besoin d'un int pas d'un string les éléments dans arvg sont stockés en string

--- client.c
+++ client.c
@@ -13,17 +13,21 @@ int main(int argc, char const *argv[]) {
     char pseudo[size_of_pseudo];            // on initialise la taille d'un string pour le pseudo
     strcpy(pseudo,argv[1]);                // on récupère le pseudo des paramètres et on le mets dans pseudo
 
+
     int size_of_ip_serveur= strlen(argv[2]); // on récupère la taille du pseudo
     char ip_serveur[size_of_ip_serveur];            // on initialise la taille d'un string pour le pseudo
     strcpy(ip_serveur,argv[2]);
 
-    int size_of_port= strlen(argv[3]); // on récupère la taille du pseudo
-    char port[size_of_port];            // on initialise la taille d'un string pour le pseudo
-    strcpy(port,argv[3]);
+    int size_of_port_= strlen(argv[3]); // on récupère la taille du pseudo
+    char port_[size_of_port_];            // on initialise la taille d'un string pour le pseudo
+    strcpy(port_,argv[3]);
+    long int value_port = strtol(port_, NULL, 10);
+    long int port=value_port;
 
-    printf("ip serveur apres: %s \n",ip_serveur); //TODO convertir le string en int...
-    printf("port : %s \n",port);                  //TODO pareil ici convertir en int...
+    printf("ip serveur : %s \n",ip_serveur);
+    printf("port : %ld \n",*&port);
     printf("pseudo : %s \n",pseudo);              //TODO convertir en char*
+
 
     // variables pour le message
     size_t longueur_du_message;
