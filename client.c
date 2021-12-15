a73d4b5aa12ae738a8aef9c0d06698bffbfa2280
DavePk04 <73966683+DavePk04@users.noreply.github.com>
2021-12-15 09:08:43 +0100

    select server and multi threads client

--- client.c
+++ client.c
@@ -1,22 +1,104 @@
-#include <arpa/inet.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
-#include <sys/socket.h>
+#include <signal.h>
 #include <unistd.h>
-#include "common.h"
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <pthread.h>
+#include <time.h>
+
+#define LENGTH 2048
+
+// Global variables
+volatile sig_atomic_t flag = 0;
+int sockfd = 0;
+char pseudo[32];
 
 
+// struct message
+// {
+// 	size_t msg_lenght;
+// 	time_t timestamp;
+// 	timestamp = time(NULL);
+// 	char* message[32];
+// };
 
-int main(int argc, char const *argv[]) {
-    //parsing pour récupérer le pseudo, l'ip_serveur et le port
-    int size_of_pseudo= strlen(argv[1]); // on récupère la taille du pseudo
-    char pseudo[size_of_pseudo];            // on initialise la taille d'un string pour le pseudo
+//oblige le système à vider le tampon associé au flux de sortie spécifié
+void str_overwrite_stdout() {
+  printf("%s", "> ");
+  fflush(stdout);
+}
+
+// remplace le retour a la ligne par le caractere de fin de ligne
+void str_trim_lf (char* arr, int length) {
+  int i;
+  for (i = 0; i < length; i++) { // trim \n
+    if (arr[i] == '\n') {
+      arr[i] = '\0';
+      break;
+    }
+  }
+}
+
+void catch_ctrl_c_and_exit(int sig) {
+    flag = 1;
+}
+
+void send_msg_handler() {
+  char message[LENGTH] = {};
+	char buffer[LENGTH + 32] = {};
+
+  while(1) {
+  	str_overwrite_stdout();       // Optionnel
+    fgets(message, LENGTH, stdin);   // recupere le msg entered in stdin
+    str_trim_lf(message, LENGTH);
+
+    if (strcmp(message, "exit") == 0) {   // optionnel
+			break;
+    } else {
+      sprintf(buffer, "%s: %s\n", pseudo, message);  // stocke le msg "%s: %s\n" dans buffer(le tampon)
+      send(sockfd, buffer, strlen(buffer), 0);
+    }
+
+		bzero(message, LENGTH);
+    bzero(buffer, LENGTH + 32);
+  }
+  catch_ctrl_c_and_exit(2);
+}
+
+void recv_msg_handler() {
+	char message[LENGTH] = {};
+  while (1) {
+		int receive = recv(sockfd, message, LENGTH, 0);
+    if (receive > 0) {
+      printf("%s\n", message);  // affiche le msg send by server
+      str_overwrite_stdout();
+    } else if (receive == 0) {
+			break;
+    } else {
+			// -1
+		}
+		memset(message, 0, sizeof(message));  // optionnel
+  }
+}
+
+
+int main(int argc, char **argv){
+	printf("args : %d\n", argc);
+	if(argc != 4){
+		printf("Usage: %s <port>\n", argv[0]);
+		return EXIT_FAILURE;
+	}
+
+	int size_of_pseudo= strlen(argv[1]); // on récupère la taille du pseudo
     strcpy(pseudo,argv[1]);                // on récupère le pseudo des paramètres et on le mets dans pseudo
 
-
-    int size_of_ip_serveur= strlen(argv[2]); // on récupère la taille du pseudo
-    char ip_serveur[size_of_ip_serveur];            // on initialise la taille d'un string pour le pseudo
-    strcpy(ip_serveur,argv[2]);
+    int size_of_ip_server= strlen(argv[2]); // on récupère la taille du pseudo
+    char ip_server[size_of_ip_server];            // on initialise la taille d'un string pour le pseudo
+    strcpy(ip_server,argv[2]);
 
     int size_of_port_= strlen(argv[3]); // on récupère la taille du pseudo
     char port_[size_of_port_];            // on initialise la taille d'un string pour le pseudo
@@ -24,34 +106,64 @@ int main(int argc, char const *argv[]) {
     long int value_port = strtol(port_, NULL, 10);
     long int port=value_port;
 
-    printf("ip serveur : %s \n",ip_serveur);
+    printf("ip serveur : %s \n",ip_server);
     printf("port : %ld \n",*&port);
     printf("pseudo : %s \n",pseudo);              //TODO convertir en char*
 
+	signal(SIGINT, catch_ctrl_c_and_exit);   //TODO signal CTRL+D
 
-    // variables pour le message
-    size_t longueur_du_message;
-    time_t timestamp;
-    char message[1024];
-    char recvmessage[1024];
-
-    int sock = checked(socket(AF_INET, SOCK_STREAM, 0));
-    struct sockaddr_in serv_addr;
-    serv_addr.sin_family = AF_INET;
-//    serv_addr.sin_port = htons(port); // on mets le port qui a été entré en paramètre lors de l'appel
-
-    // Conversion de string vers IPv4 ou IPv6 en binaire
-    checked(inet_pton(AF_INET, (const char *)ip_serveur, &serv_addr.sin_addr)); // on remplace l'ip par celui qui a été donné en paramtre
-
-    checked(connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)));
+	// TODO name = argv[1] "pseudo"
+  str_trim_lf(pseudo, strlen(pseudo));
 
 
+	if (strlen(pseudo) > 32 || strlen(pseudo) < 2){
+		printf("Name must be less than 30 and more than 2 characters.\n");
+		return EXIT_FAILURE;
+	}
+	printf("welcome : %s", pseudo);
 
-    while (fgets(message, 1024, stdin)) {
-        longueur_du_message = strlen(message);
-        checked(write(sock, message, longueur_du_message + 1));
-        ssize_t nbytes = checked(read(sock, recvmessage, 1024));
-        printf("Client received %s\n", recvmessage);
+	struct sockaddr_in server_addr;
+
+	/* Socket settings */
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+  server_addr.sin_family = AF_INET;
+  server_addr.sin_addr.s_addr = inet_addr(ip_server);
+  server_addr.sin_port = htons(port);
+
+
+  // Connect to Server
+  int err = connect(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr));
+  if (err == -1) {
+		printf("ERROR: connect\n");
+		return EXIT_FAILURE;
+	}
+
+	// Send name
+	send(sockfd, pseudo, 32, 0);   // send pseudo to server
+
+	printf("=== WELCOME TO THE CHATROOM ===\n");   // optionnel
+
+	pthread_t send_msg_thread;
+
+  if(pthread_create(&send_msg_thread, NULL, (void *) send_msg_handler, NULL) != 0){
+		printf("ERROR: pthread\n");
+    return EXIT_FAILURE;
+	}
+
+	pthread_t recv_msg_thread;
+  if(pthread_create(&recv_msg_thread, NULL, (void *) recv_msg_handler, NULL) != 0){
+		printf("ERROR: pthread\n");
+		return EXIT_FAILURE;
+	}
+
+	while (1){
+		if(flag){
+			printf("\nBye\n");
+			break;
     }
-    return 0;
+	}
+
+	close(sockfd);
+
+	return EXIT_SUCCESS;
 }
--- serveur.c
+++ serveur.c
@@ -1,79 +1,112 @@
-// Example code: A simple server side code, which echos back the received message.
-// Handle multiple socket connections with select and fd_set on Linux
-#include <netinet/in.h>
-#include <stdbool.h>
+#include <sys/types.h>
+#include <sys/socket.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>  //strlen
-#include <sys/socket.h>
-#include <sys/time.h>  //FD_SET, FD_ISSET, FD_ZERO macros
-#include <unistd.h>    //close
-
-#include "common.h"
-
-// TODO: gerer les erreurs lors du parsing des arguments
-int main(int argc, char *argv[]) {
-
-
-
-    //parsing pour récupérer le port
-    unsigned const long int port = (unsigned long int) (argv[0]);
-    //TODO: fonction qui vérifie si le port entré est correcte --> https://wallu.pagesperso-orange.fr/pag-ports.htm max 65535
-    int opt = 1;
-    int master_socket = checked(socket(AF_INET, SOCK_STREAM, 0));
-    checked(setsockopt(master_socket, SOL_SOCKET, SO_REUSEADDR, (char *) &opt, sizeof(opt)));
-
-    // type of socket created
-    struct sockaddr_in address;
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons(port);
-
-    checked(bind(master_socket, (struct sockaddr *) &address, sizeof(address)));
-    checked(listen(master_socket, 3));
-
-    size_t addrlen = sizeof(address);
-    fd_set readfds;
-    int clients[1024];   // like fds table
-    int nclients = 0;    // on doit definir ici le nbre de clients qui vont se connecter
-
-    while (true) {
-        FD_ZERO(&readfds);
-        // Etape d'ajout du socket server et de tous les sockets clients dans le fd_set
-        FD_SET(master_socket, &readfds);
-        int max_fd = master_socket;
-        for (int i = 0; i < nclients; i++) {
-            FD_SET(clients[i], &readfds);
-            if (clients[i] > max_fd) {
-                max_fd = clients[i];
-            }
-        }
-        // wait for an activity on one of the sockets, timeout is NULL
-        select(max_fd + 1, &readfds, NULL, NULL, NULL);
-
-        if (FD_ISSET(master_socket, &readfds)) {
-            // Si c'est le master socket qui a des donnees, c'est une nouvele connexion.
-            clients[nclients] = accept(master_socket, (struct sockaddr *) &address, (socklen_t *) &addrlen);
-            nclients++;
-        } else {
-            // Sinon, c'est un message d'un client
-            for (int i = 0; i < nclients; i++) {
-                if (FD_ISSET(clients[i], &readfds)) {
-                    char *buffer;
-                    size_t nbytes = receive(clients[i], (void *) &buffer);
-                    if (nbytes > 0) {  // closed
-                        ssend(clients[i], buffer, nbytes);
-                        free(buffer);
-                    } else {
-                        close(clients[i]);
-                        // On deplace le dernier socket a la place de libre pur ne pas faire de trou.
-                        clients[i] = clients[nclients - 1];
-                        nclients--;
-                    }
-                }
-            }
-        }
-    }
-
-    return 0;
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <unistd.h>
+ 
+ 
+int main(int argc, char **argv)
+{
+  int sockfd, sockfd2;          // descripteurs de socket
+  fd_set readfds;               // ensemble des descripteurs en lecture qui seront surveilles par select
+  int t[FD_SETSIZE];            // tableau qui contiendra tous les descripteurs de sockets, avec une taille egale a la taille max de l'ensemble d'une structure fd_set
+  int taille=0;                 // nombre de descripteurs dans le tableau precedent
+  char buf[1024];               // espace necessaire pour stocker le message recu
+ 
+  memset(buf,'\0',1024);        // initialisation du buffer qui sera utilisé
+ 
+  struct sockaddr_in my_addr;   // structure d'adresse qui contiendra les param reseaux du recepteur
+  struct sockaddr_in client;    // structure d'adresse qui contiendra les param reseaux de l'expediteur
+ 
+  // taille d'une structure sockaddr_in utile pour la fonction recvfrom
+  socklen_t sin_size = sizeof(struct sockaddr_in);
+ 
+  // verification du nombre d'arguments sur la ligne de commande
+  if(argc != 2)
+  {
+    printf("Usage: %s port_local\n", argv[0]);
+    exit(-1);
+  }
+ 
+  // creation de la socket
+  sockfd = socket(AF_INET,SOCK_STREAM,IPPROTO_TCP);
+ 
+  // initialisation de la structure d'adresse du serveur (pg local)
+ 
+  // famille d'adresse
+  my_addr.sin_family = AF_INET;
+ 
+  // recuperation du port du serveur
+  my_addr.sin_port = ntohs(atoi(argv[1]));
+ 
+  // adresse IPv4 du serveur
+  my_addr.sin_addr.s_addr=htonl(INADDR_ANY);
+ 
+  // association de la socket et des param reseaux du serveur
+  if(bind(sockfd,(struct sockaddr*)&my_addr,sizeof(my_addr)) != 0)
+  {
+    perror("Erreur lors de l'appel a bind -> ");
+    exit(1);
+  }
+ 
+  // indication de la limite MAX de la file d'attente des connexions entrantes
+  if(listen(sockfd,10) != 0)
+  {
+    perror("Erreur lors de l'appel a listen -> ");
+    exit(2);
+  }
+ 
+  printf("Attente de connexion\n");
+ 
+  t[0]=sockfd;    // on ajoute deja la socket d'ecoute au tableau de descripteurs
+  taille++;       // et donc on augmente "taille"
+ 
+  while(1){              
+            FD_ZERO(&readfds);                                        //il faut remettre tt les elements ds readfds a chaque recommencement de la boucle, vu que select modifie les ensembles
+            int j;
+            int sockmax=0;
+            for(j=0;j<taille;j++){
+                                   if(t[j] != 0)             
+                                              FD_SET(t[j],&readfds);  // on remet donc tous les elements dans readfds
+                                   if(sockmax < t[j])                 // et on prend ici le "numero" de socket maximal pour la fonction select
+                                              sockmax = t[j];
+                                   }
+                                  
+            if(select(sockmax+1,&readfds,NULL,NULL,NULL) == -1){      // on utilise le select sur toutes les sockets y compris celle d'ecoute
+                                          perror("Erreur lors de l'appel a select -> ");
+                                          exit(1);
+                                          }
+                                           
+            if(FD_ISSET(sockfd,&readfds)){                            // si la socket d'ecoute est dans readfds, alors qqch lui a ete envoye (=connection d'un client)
+                                          if((sockfd2 = accept(sockfd,(struct sockaddr*)&client,&sin_size)) == -1){     // on accepte la connexion entrante et on cree une socket...
+                                                      perror("Erreur lors de accept -> ");
+                                                      exit(3);
+                                                      }
+                                          printf("Connexion etablie avec %s\n", inet_ntoa(client.sin_addr));
+                                          taille++;                                                                     // ...qui est donc ajoutee au tableau de descripteurs
+                                          t[taille-1]=sockfd2;
+                                          }
+            int i;
+            for(i=1;i<taille;i++){                                     // on parcourt tous les autres descripteurs du tableau
+                            if(FD_ISSET(t[i],&readfds)){               // si une socket du tableau est dans readfds, alors qqch a ete envoye au serveur par un client
+                                                        if(recv(t[i],&buf,1024,0) == -1){                                         // on stocke alors le message
+                                                                                     perror("Erreur lors de la reception -> ");
+                                                                                     exit(4);
+                                                                                     }
+                                                        printf("La chaine recue est: %s\n",buf);                                  // et on l'affiche
+                                                        int k;
+                                                        for(k=1;k<taille;k++){                                                    // puis on l'envoie a tous les clients...
+                                                                              if(k != i){                                         // ...sauf l'envoyeur
+                                                                                   if(send(t[k],&buf,1024,0) == -1){             
+                                                                                                             perror("Erreur lors de l'appel a send -> ");
+                                                                                                             exit(1);
+                                                                                                             }
+                                                                                   }
+                                                                              }
+                                                        }              // reste donc a resoudre le probleme de supprimer la socket du tableau, et modifier "taille"...
+                            }
+           }
 }
